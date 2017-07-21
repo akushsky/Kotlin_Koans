1a2c465c272b5b86dd863bb86819ded4aa2d262b
akushsky <akushsky@it-universe.ru>
2017-07-21 22:15:57 +0300

    Range tasks

--- lesson2/task4/src/Task.kt
+++ lesson2/task4/src/Task.kt
@@ -1,4 +1,22 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return MyDateIterator(this)
+    }
+}
+
+class MyDateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var currentDate: MyDate = dateRange.start
+
+    override fun hasNext(): Boolean {
+        return currentDate <= dateRange.end
+    }
+
+    override fun next(): MyDate {
+        val retDate: MyDate = currentDate
+        currentDate = retDate.nextDay()
+        return retDate
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
